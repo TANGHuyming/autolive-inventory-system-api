5af0650b61d88d40e1c474949e3d571a44192fd2
TANGHuyming <2023277tang@aupp.edu.kh>
2026-09-05 13:57:59 +0000

    Configure scout to stop using queue

--- config/scout.php
+++ config/scout.php
@@ -46,11 +46,11 @@ return [
     |
     */
 
-    // 'queue' => env('SCOUT_QUEUE', false),
-    'queue' => [
-        'connection' => 'redis',
-        'queue' => 'scout',
-    ],
+    'queue' => env('SCOUT_QUEUE', false),
+    // 'queue' => [
+    //     'connection' => 'redis',
+    //     'queue' => 'scout',
+    // ],
 
     /*
     |--------------------------------------------------------------------------
