fddbe8af1caa0fe4f6b440677a23a64e348f7670
TANGHuyming <2023277tang@aupp.edu.kh>
2026-09-06 01:17:06 +0000

    Add database throttling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->statefulApi();
-        $middleware->throttleWithRedis();
+        // $middleware->throttleWithRedis();
+        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
